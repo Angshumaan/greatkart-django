32f9754754f0dd6c2e4b1e5f0c4f686802b8c309
= <=>
2021-06-06 10:36:04 +0530

    all

--- orders/views.py
+++ orders/views.py
@@ -2,12 +2,31 @@ from django.http.response import HttpResponse
 from django.shortcuts import redirect, render, HttpResponse
 from carts.models import CartItem
 from .forms import OrderForm
-from .models import Order
+from .models import Order, Payment
 import datetime
+import json
 # Create your views here.
 
 
 def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(
+        user=request.user, is_ordered=False, order_number=body['orderID'])
+    print(body)
+
+    # store transaction details inside payment model from javascript fetch
+    payment = Payment(
+        user=request.user,
+        payment_id=body['transID'],
+        payment_method=body['payment_method'],
+        amount_paid=order.order_total,
+        status=body['status'],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
     return render(request, 'orders/payments.html')
 
 
